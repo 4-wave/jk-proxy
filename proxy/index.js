07d8f9755818a4237e05ff331fc6a566b6287ceb
jonathanhkim <jhkim792@gmail.com>
2020-01-09 12:30:08 -0800

    working version of proxy

--- proxy/index.js
+++ proxy/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const proxy = require('http-proxy-middleware');
 const port = 8080;
 
-app.use('/*', express.static(path.resolve(__dirname, '../public')));
+app.use('/', express.static(path.resolve(__dirname, '../public')));
+app.use('/listing/:id', express.static(path.resolve(__dirname, '../public')));
+
+
+app.use('/*', proxy({ target: 'http://localhost:3003/', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`listening to port ${port}...`);
